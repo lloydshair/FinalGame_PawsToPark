a78031bf112fcb568051e8879f8030beaa3652fa
Setaish <Setaish1qayomi@gmail.com>
2024-04-08 21:01:47 -0400

    ekeyfixed

--- Assets/Scripts/level2hams2.cs
+++ Assets/Scripts/level2hams2.cs
@@ -189,7 +189,7 @@ public class level2hams2 : MonoBehaviour
 
     void UpdateHiding()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.RightControl))
         {
             isHidden = !isHidden;
             custRender.enabled = !isHidden;
